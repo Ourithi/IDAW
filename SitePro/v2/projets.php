471e637a523629698ac0d94d625bcbc3da723cf9
Ourithi <thibault.ouriachi@gmail.com>
2023-10-10 15:39:44 +0200

    commit final v2

--- SitePro/v2/projets.php
+++ SitePro/v2/projets.php
@@ -12,6 +12,8 @@
         <h2>Projets</h2>
 
         <p><a href="http://localhost/IDAW/SitePro/v1/index.html">Projet IDAW</a></p>
+        <p> Dévellopement d'une version mobile (applicative) de l'extension de navigateur Bourgad </p>
+        <p> Membre du pôle animation du BDE Men IMT Black (2021-2022)</p>
     </div>
 
 <?php

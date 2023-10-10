ffc58f31ddf9b023b5bf1ddffac7bd593d21d8c4
Ourithi <thibault.ouriachi@gmail.com>
2023-10-10 17:00:51 +0200

    fix langues

--- SitePro/v3/en/template_menu.php
+++ SitePro/v3/en/template_menu.php
@@ -1,23 +1,28 @@
 <?php
-    function renderMenuToHTML($currentPageId) {
+    function renderMenuToHTML($currentPageId,$lang) {
         // un tableau qui d\'efinit la structure du site
-        $mymenu = array(
+        $i=0;
+        if($lang=='en'){
+            $i=1;
+        }
+        $url='http://localhost/IDAW/SitePro/v3/'.$lang.'/index.php?lang='.$lang.'&page=';
             // idPage titre
-            'accueil' => array( 'Accueil' ),
-            'cv' => array( 'CV' ),
-            'projets' => array('Mes Projets'),
-            'hobbies' => array('Mes hobbies'),
-            'infos-techniques' => array('Infos Techniques'),
-            'contact' => array('Mes informations de contact')
-            );
+        $mymenu = array(
+            'accueil' => array( 'Accueil','Welcome page' ),
+            'cv' => array( 'CV','Resume' ),
+            'projets' => array('Mes Projets','My projects'),
+            'hobbies' => array('Mes hobbies','My hobbies'),
+            'infos-techniques' => array('Infos Techniques','Technical information'),
+            'contact' => array('Mes informations de contact','Contact information')
+        );
         echo "<nav class=menu>";
         echo "<ul>";
         foreach($mymenu as $pageId => $pageParameters) {
             if($currentPageId==$pageId) {
-                echo '<li><a id=currentpage href="http://localhost/IDAW/SitePro/v3/index.php?page="'.$pageId.'">'.$pageParameters[0] .'</a></li>';
+                echo '<li><a id=currentpage href="'.$url.$pageId.'">'.$pageParameters[$i] .'</a></li>';
             }
             else {
-                echo '<li><a href="http://localhost/IDAW/SitePro/v3/index.php?page='.$pageId.'">'.$pageParameters[0].' </a></li>';
+                echo '<li><a href="'.$url.$pageId.'">'.$pageParameters[$i].' </a></li>';
             }
         }
         echo "</ul>";

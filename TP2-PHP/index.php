eba0b6841d5519567715421f593b5c7af979f89a
Ourithi <thibault.ouriachi@gmail.com>
2023-10-10 14:18:06 +0200

    commit php et init v2

---
+++ TP2-PHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    </head>
+
+    <body>
+        Il est:
+        <?php 
+        date_default_timezone_set('Europe/Paris');
+        echo date ('G \h i \m\i\n s \s');
+        ?>
+
+    </body>
+
+</html>

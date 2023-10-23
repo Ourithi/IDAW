ea6754f28edec813d343ccf8c9c3026ca2b9c8d7
Ourithi <thibault.ouriachi@gmail.com>
2023-10-23 21:21:36 +0200

    api login etc

--- Projet/backend/user.php
+++ Projet/backend/user.php
@@ -8,54 +8,84 @@ header('Content-Type: application/json');
 header("Cache-Control: no-cache");
 
 require_once('config.php');
-require_once('db_conn.php');
+require_once('init_pdo.php');
 $method=$_SERVER['REQUEST_METHOD'];
 
 switch($method){
     case 'GET':
         //on initialise une liste d'user vide
-        $users=array();
-        $query=$pdo->prepare('select * from `users`');
-        $success=$query->execute();
-
-        if($success){
-            //on loop sur toutes les lignes du tableau et on fait un array par user qu'on met dans un array qui les contient tous
-            while($ligne=($query->fetch(PDO::FETCH_OBJ))){
-                $user=array(
-                    "id"=> $ligne->id,
-                    "name"=> $ligne->name,
-                    "email"=>$ligne->email
-                );
-                array_push($users,$user);
+        $get=json_decode(file_get_contents('php://input'),true);
+        //check params pour login
+        if(isset($get['name']) && isset($get['pwd'])){
+            $name=$get['name'];
+            $pwd=$get['pwd'];
+            $query=$pdo->prepare('SELECT `id` from `users` where `name`="'.$name.'"and `pwd`="'.$pwd.'"');
+            $success=$query->execute();
+            //check succès query
+            if ($success){
+                $id=$query->fetch(PDO::FETCH_OBJ);
+                //check match user/pwd
+                if ($id){
+                    http_response_code(200);
+                    echo json_encode(array("message"=> "Success"));
+                }
+                else{
+                    http_response_code(200);
+                    echo json_encode(array("message"=> "Mauvais nom d'utilisateur/mot de passe"));
+                }
             }
-            http_response_code(200);
-            echo json_encode($users);
+            else{
+                http_response_code(500);
+                echo json_encode(array("message"=> "Internal server error"));
+            }
+        }
+        elseif (isset($get['id'])){
+            $id=$get['id'];
+            $query=$pdo->prepare('SELECT * from `users` where `id`="'.$id.'"');
+            $success=$query->execute();
+            if ($success){
+                $user=$query->fetch(PDO::FETCH_OBJ);
+                if ($user){
+                    echo json_encode($user);
+                    http_response_code(200);
+                }
+                else{
+                    http_response_code(400);
+                    echo json_encode(array("message"=> "Bad request"));
+                }
+
+            }
+            else{
+                http_response_code(500);
+                echo json_encode(array("message"=> "Internal server error"));
+            }
         }
         else{
-            http_response_code(500);
-            echo json_encode(array("message"=> "Internal server error"));
+            http_response_code(400);
+            echo json_encode(array("message"=> "Bad request"));
         }
+        
+        break;
 
 
     case 'POST':
 
         $post=json_decode(file_get_contents('php://input'),true);
-        if(isset($post['user']) && isset($post['email'])){
-            $user=$post['user'];
-            $email=$post['email'];
-            $query=$pdo->prepare('INSERT INTO `users`(`name`, `email`) VALUES ("'.$user.'","'.$email.'")');
+        if(isset($post['name']) && isset($post['pwd']) && isset($post['taille']) && isset($post['poids']) && isset($post['age']) && isset($post['sexe'])){
+            $name=$post['name'];
+            $pwd=$post['pwd'];
+            $taille=$post['taille'];
+            $poids=$post['poids'];
+            $age=$post['age'];
+            $sexe=$post['sexe'];
+            $query=$pdo->prepare('INSERT INTO `users`( `name`, `pwd`, `taille`, `poids`, `age`, `sexe`) VALUES ("'.$name.'","'.$pwd.'","'.$taille.'","'.$poids.'","'.$age.'","'.$sexe.'")');
             $success=$query->execute();
             if($success){
-                $query=$pdo->prepare('SELECT * FROM `users`WHERE `email`="'.$email.'"');
+                $query=$pdo->prepare('SELECT max(id) AS id FROM `users`');
                 $success=$query->execute();
                 if($success){
-                    $user=$query->fetch(PDO::FETCH_OBJ);
-                    $user=array(
-                        "id"=> $user->id,
-                        "name"=> $user->name,
-                        "email"=>$user->email
-                    );
-                    echo json_encode($user);
+                    $id=$query->fetch(PDO::FETCH_OBJ);
+                    echo json_encode($id);
                     http_response_code(201);
                 }
                 else{
@@ -72,6 +102,7 @@ switch($method){
             http_response_code(400);
             echo json_encode(array("message"=> "Bad request"));
         }
+        break;
 
 
     case 'PUT':
@@ -114,6 +145,7 @@ switch($method){
             http_response_code(400);
             echo json_encode(array("message"=> "Bad request"));
         }
+        break;
 
     case 'DELETE':
         $del=json_decode(file_get_contents('php://input'), true);
@@ -136,8 +168,10 @@ switch($method){
             http_response_code(400);
             echo json_encode(array("message"=> "Bad request"));
         }
+        break;
 
-    case default:
+    default:
             http_response_code(405);
             echo json_encode(array("message"=> "Method not allowed"));
+            break;
 }

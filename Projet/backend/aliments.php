3e10db8ff19cd8c3c9b32230b9bf3ad1cd5bf607
Ourithi <thibault.ouriachi@gmail.com>
2023-10-26 19:00:51 +0200

    create aliment

--- Projet/backend/aliments.php
+++ Projet/backend/aliments.php
@@ -42,22 +42,33 @@ switch($method){
     case 'POST':
 
         $post=json_decode(file_get_contents('php://input'),true);
-        if(isset($post['aliment']) && isset($post['email'])){
-            $aliment=$post['aliment'];
-            $email=$post['email'];
-            $query=$pdo->prepare('INSERT INTO `aliments`(`name`, `email`) VALUES ("'.$aliment.'","'.$email.'")');
+        $queryFields = "(";
+        $queryValues = "(";
+        if( isset($post['nom_aliment']) && isset($post['energie']) && isset($post['glucides']) && isset($post['lipides']) && isset($post['sucre']) && isset($post['fibres']) && isset($post['proteines']) && isset($post['sel'])){
+            foreach ($post as $key => $value) {
+                if($key !='id_aliment'){
+                    $queryFields = $queryFields."`".$key."`,";
+                    $queryValues = $queryValues."'".$value."',";
+                }
+                else{
+                    //$id_aliment=$key;
+                }
+            }
+            $queryFields = substr($queryFields, 0, -1);
+            $queryValues = substr($queryValues, 0, -1);
+            $queryFields = $queryFields.")";
+            $queryValues = $queryValues.")";
+            $queryString = "INSERT INTO `aliment` ".$queryFields." VALUES ".$queryValues;
+            //echo json_encode($queryString);
+
+            $query=$pdo->prepare($queryString);
             $success=$query->execute();
             if($success){
-                $query=$pdo->prepare('SELECT * FROM `aliments`WHERE `email`="'.$email.'"');
+                $query=$query=$pdo->prepare('SELECT max(id_aliment) AS id_aliment FROM `aliment`');
                 $success=$query->execute();
                 if($success){
-                    $aliment=$query->fetch(PDO::FETCH_OBJ);
-                    $aliment=array(
-                        "id"=> $aliment->id,
-                        "name"=> $aliment->name,
-                        "email"=>$aliment->email
-                    );
-                    echo json_encode($aliment);
+                    $id_aliment=$query->fetch(PDO::FETCH_OBJ);
+                    echo json_encode($id_aliment);
                     http_response_code(201);
                 }
                 else{
